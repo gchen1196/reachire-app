3909cc3d239c4ef44631e2f159e564f9550a23c2
George Chen <gchen1196@gmail.com>
2026-01-08 19:06:53 -0800

    hotfix fix build error

--- src/components/billing/index.ts
+++ src/components/billing/index.ts
@@ -1,2 +1,2 @@
 export { PlanSelector, PLANS, CREDIT_PACKS } from './PlanSelector'
-export type { Plan, CreditPackConfig } from './PlanSelector'
+export type { PlanConfig, CreditPackConfig, SubscribablePlan } from './PlanSelector'
